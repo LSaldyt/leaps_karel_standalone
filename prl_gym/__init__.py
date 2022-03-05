f6bb751a7e10812e832aa4d605ca0fa3d5a74eb0
LSaldyt <lucassaldyt@gmail.com>
2022-03-05 12:45:41 -0700

    Remove LEAPS-specific code & data

--- prl_gym/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-from gym.envs.registration import register
-
-logger = logging.getLogger(__name__)
-
-register(
-        id='CartPoleDiscrete-v0',
-        entry_point='prl_gym.envs:CartPoleDiscreteEnv',
-)
